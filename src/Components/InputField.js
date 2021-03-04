332e1691788773c8726b279140b553cde6d6f6cb
saideep1011 <saideep2311@gmail.com>
2021-03-04 22:18:20 +0530

    init project

---
+++ src/Components/InputField.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function InputField() {
+  return (
+    <div>
+      <form>
+        <input type="text" name="todo" placeholder="Add your New Todo" />
+      </form>
+    </div>
+  );
+}
